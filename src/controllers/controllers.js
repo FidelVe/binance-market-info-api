be8e0409dfcc11058a4484c581fc7359055ba6ae
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-11-02 17:52:20 -0400

    implemented /api/v1/pairs/:pair' endpoint

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,30 +1,40 @@
 const utils = require("../utils/utils");
 
 /**
- * route: /api/v1/pairs
+ * route: /api/v1/pairs/:pairs
  */
 const getPair = async (req, res) => {
   //
   console.log("request to '/api/v1/pairs/:pair'");
+
   res.set("Connection", "close").status(200);
+  const response = {
+    result: null
+  };
+
+  if (utils.pairs.includes(req.params.pair)) {
+    const copyOfOrderBook = utils.copyOrderBook(req.inMemoryOrderBook);
+    const topOrders = utils.getTopOrders(copyOfOrderBook);
+    response.result = {
+      bids: topOrders[req.params.pair].bids,
+      asks: topOrders[req.params.pair].asks
+    };
+  } else {
+    response.result = `Pair "${req.params.pair}" unsupported`;
+  }
+  console.log(req.params);
 
   // response
-  res.json({
-    result: utils.pairs
-  });
+  res.json(response);
   res.end();
 };
 
 /**
- * route: /api/v1/pairs/:pair
+ * route: /api/v1/pairs
  */
 const getPairs = async (req, res) => {
   //
   console.log("request to '/api/v1/pairs'");
-  console.log("inMemoryOrderBook");
-  const copyOfOrderBook = utils.copyOrderBook(req.inMemoryOrderBook);
-  const topOrders = utils.getTopOrders(copyOfOrderBook);
-  console.log(topOrders);
   res.set("Connection", "close").status(200);
 
   // response
